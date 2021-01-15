eba38bf5bfcf785d246d2760b09c7dc54c1b9dcc
qoob <qoob@national.shitposting.agency>
2021-01-15 14:44:18 -0800

    init: added support for stivale2 firmware tag

--- src/init.c
+++ src/init.c
@@ -1,7 +1,6 @@
 #include <mem.h>
 #include <protos/stivale2.h>
 #include <sys/interrupts.h>
-#include <drivers/vesa.h>
 #include <virt/intel/vmx/vmx.h>
 #include <ospm/ospm.h>
 
@@ -13,8 +12,10 @@ void sonar_main(struct stivale2_struct* info) {
     struct stivale2_struct_tag_framebuffer* fb;
     struct stivale2_struct_tag_rsdp*        rsdp;
     struct stivale2_struct_tag_smp*         smp;
+    struct stivale2_struct_tag_firmware*    firmware;
 
-    for (struct stivale2_tag* cur = (struct stivale2_tag *)info->tags; cur; cur = cur->next) {
+    for (struct stivale2_tag* cur = (struct stivale2_tag *)info->tags; cur;
+            cur = (struct stivale2_tag *)(cur->next)) {
         switch (cur->identifier) {
             case STIVALE2_STRUCT_TAG_MEMMAP_ID:
                 memmap = (struct stivale2_struct_tag_memmap *)cur;
@@ -28,14 +29,22 @@ void sonar_main(struct stivale2_struct* info) {
             case STIVALE2_STRUCT_TAG_SMP_ID:
                 smp = (struct stivale2_struct_tag_smp *)cur;
                 break;
+            case STIVALE2_STRUCT_TAG_FIRMWARE_ID:
+                firmware = (struct stivale2_struct_tag_firmware *)cur;
+                break;
         }
     }
     
     init_isrs();
-    init_vesa(fb);
-    init_mem(memmap);
 
-    init_ospm();
+    enable_early_log(fb);
+    TRACE("Booted on \"%s\" by \"%s\" (version \"%s\")\n",
+            firmware->flags & 1 ? "BIOS" : "UEFI", 
+            info->bootloader_brand,
+            info->bootloader_version);
+
+    init_mem(memmap);
+    init_ospm(rsdp, smp);
     init_vmx();
     
     asm volatile("cli\n\t"
